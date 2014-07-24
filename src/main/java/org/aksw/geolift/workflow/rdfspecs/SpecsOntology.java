7199e153e07e76ec08647ba633e548c1409c191e
MSherif <mohameddotsherif@gmail.com>
2014-07-24 17:27:35 +0200

    add RDFConfigReader

---
+++ src/main/java/org/aksw/geolift/workflow/rdfspecs/SpecsOntology.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.aksw.geolift.workflow.rdfspecs;
+
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.vocabulary.RDF;
+
+/**
+ * @author sherif
+ *
+ */
+public class SpecsOntology {
+	public static final String uri = "http://geoknow.org/specsontology/";
+
+	private static Property property(String name) {
+		Property result = ResourceFactory.createProperty(uri + name);
+		return result;
+	}
+
+	protected static final Resource resource( String local ){ 
+		return ResourceFactory.createResource( uri + local ); 
+	}
+
+	public static String getURI(){ return uri;	}
+
+	public static final Property hasParameter	= property("hasParameter");
+	public static final Property hasKey 		= property("hasKey");
+	public static final Property hasValue 	= property("hasValue");
+	public static final Property hasModule 	= property("hasModule");
+	public static final Property hasOperator 	= property("hasOperator");
+	public static final Property hasInput 	= property("hasInput");
+	public static final Property hasOutput 	= property("hasOutput");
+	public static final Property nextStep 	= property("nextStep");
+	public static final Property isFirstStep 	= property("isFirstStep");
+	public static final Property isLastStep 	= property("isLastStep");
+	
+	public static final Resource Step 						= resource( "Step" );
+	public static final Resource Dataset 						= resource( "Dataset" );
+	public static final Resource Module 						= resource( "Module" );
+	public static final Resource DereferencingModule 			= resource( "DereferencingModule" );
+	public static final Resource LinkingModule 				= resource( "LinkingModule" );
+	public static final Resource NLPModule 					= resource( "NLPModule" );
+	public static final Resource FilterModule 				= resource( "FilterModule" );
+	public static final Resource ConformationModule 			= resource( "ConformationModule" );
+	public static final Resource ModuleParameter 				= resource( "ModuleParameter" );
+	public static final Resource NLPModuleParameter 			= resource( "NLPModuleParameter" );
+	public static final Resource DereferencingModuleParameter	= resource( "DereferencingModuleParameter" );
+	public static final Resource LinkingModuleParameter 		= resource( "linkingModuleParameter" );
+	public static final Resource FilterModuleParameter 		= resource( "FilterModuleParameter" );
+	public static final Resource ConformationModuleParameter 	= resource( "ConformationModuleParameter" );
+	
+}
