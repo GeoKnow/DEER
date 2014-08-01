d96ddd9403298f80333d817bd0120b8ec092ff97
MSherif <mohameddotsherif@gmail.com>
2014-08-01 12:31:03 +0200

    the only way to run Geolift now is using the RDF config file

--- src/main/java/org/aksw/geolift/io/Writer.java
+++ src/main/java/org/aksw/geolift/io/Writer.java
@@ -6,6 +6,8 @@ package org.aksw.geolift.io;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 import com.hp.hpl.jena.rdf.model.Model;
 
 /**
@@ -13,9 +15,14 @@ import com.hp.hpl.jena.rdf.model.Model;
  *
  */
 public class Writer {
+	private static final Logger logger = Logger.getLogger(Writer.class.getName());
+	
 	public static void writeModel(Model model, String format, String outputFile) throws IOException
 	{
+		logger.info("Saving dataset to " + outputFile + "...");
+		long starTime = System.currentTimeMillis();
 		FileWriter fileWriter = new FileWriter(outputFile);
 		model.write(fileWriter, format);
+		logger.info("Saving file done in " + (System.currentTimeMillis() - starTime) +"ms.");
 	}
 }
